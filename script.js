4ec0836979e36354fa725be34d211ce86f880a48
kubzer0 <jakub.gryka@gmail.com>
2018-11-05 10:25:08 +0100

    render function added

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const ToDo = function(){
-    this.task =[]
+const ToDo = function () {
+    this.tasks = []
 }
 
-ToDo.prototype.addTask = function(){
-    this.addTask = addTask
+ToDo.prototype.addTask = function (text) {
+    this.tasks.push(new Task(text))
+    this.render()
 }
 
-const newTask= new ToDo()
+ToDo.prototype.render = function () {
+    document.body.innerHTML = ''
+    this.tasks.forEach(task => {
+        const taskDiv = document.createElement('div')
+        const taskText = document.createTextNode(task.text)
+        taskDiv.appendChild(taskText)
+        document.body.appendChild(taskDiv)
+    })
+}
+
+function Task(text) {
+    this.text = text
+}
+
+const newTask1 = new ToDo()
+newTask1.addTask('task1111')
+
+
