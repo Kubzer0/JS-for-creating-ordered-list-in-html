5d49aa6c97695f106f0941cb851bdf243f7451ee
kubzer0 <jakub.gryka@gmail.com>
2018-11-05 12:00:58 +0100

    code changed to fit class keyword

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-const ToDo = function () {
-    this.tasks = []
+class ToDo {
+    constructor() {
+        this.tasks = []
 
-    this.render()
+        this.render()
+    }
+    addTask(text) {
+        this.tasks.push(new Task(text))
+        this.render()
+    }
+    render() {
+
+        document.body.innerHTML = ''
+
+        this.makeUI()
+
+        const orderedList = document.createElement('ol')
+        this.tasks.forEach(task => {
+            const taskLi = document.createElement('li')
+            taskLi.innerText = task.text
+            orderedList.appendChild(taskLi)
+        })
+        document.body.appendChild(orderedList)
+    }
+    makeUI() {
+        const input = document.createElement('input')
+        const button = document.createElement('button')
+
+        button.addEventListener('click', () => this.addTask(input.value))
+        document.body.appendChild(input)
+        button.innerText = "dodaj do listy"
+        document.body.appendChild(button)
+    }
+
 }
 
-ToDo.prototype.addTask = function (text) {
-    this.tasks.push(new Task(text))
-    this.render()
+
+class Task {
+    constructor(text) {
+        this.text = text
+    }
 }
 
-ToDo.prototype.render = function () {
-
-    document.body.innerHTML = ''
-
-    this.makeUI()
-
-    const orderedList = document.createElement('ol')
-    this.tasks.forEach(task => {
-        const taskLi = document.createElement('li')
-        taskLi.innerText = task.text
-        orderedList.appendChild(taskLi)
-    })
-    document.body.appendChild(orderedList)
-}
-
-function Task(text) {
-    this.text = text
-}
-
-ToDo.prototype.makeUI = function () {
-    const input = document.createElement('input')
-    const button = document.createElement('button')
-
-    button.addEventListener('click', () => this.addTask(input.value))
-    document.body.appendChild(input)
-    button.innerText = "dodaj do listy"
-    document.body.appendChild(button)
-}
 
 const newTask1 = new ToDo()
 
